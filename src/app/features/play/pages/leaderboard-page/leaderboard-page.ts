80934556de07d1d8134f478b63d7ed5c81f36835
“@neliogodoi” <nelioapp@gmail.com>
2026-03-11 20:22:19 -0300

    fix ranking

--- src/app/features/play/pages/leaderboard-page/leaderboard-page.ts
+++ src/app/features/play/pages/leaderboard-page/leaderboard-page.ts
@@ -1,6 +1,7 @@
 import { AsyncPipe, NgFor, NgIf } from '@angular/common';
 import { Component, inject } from '@angular/core';
 import { limit, orderBy, query } from '@angular/fire/firestore';
+import { map } from 'rxjs/operators';
 
 import { UserStats } from '../../../../models/firestore.models';
 import { FirestoreService } from '../../../../services/firestore.service';
@@ -14,7 +15,13 @@ import { FirestoreService } from '../../../../services/firestore.service';
 export class LeaderboardPage {
   private readonly firestoreService = inject(FirestoreService);
 
-  readonly topUsers$ = this.firestoreService.col$<UserStats>('userStats', (ref) =>
-    query(ref, orderBy('totalXp', 'desc'), orderBy('streak', 'desc'), limit(20))
-  );
+  readonly topUsers$ = this.firestoreService
+    .col$<UserStats>('userStats', (ref) => query(ref, orderBy('totalXp', 'desc'), limit(50)))
+    .pipe(
+      map((users) =>
+        [...users]
+          .sort((a, b) => (b.totalXp - a.totalXp) || ((b.streak ?? 0) - (a.streak ?? 0)))
+          .slice(0, 20)
+      )
+    );
 }
